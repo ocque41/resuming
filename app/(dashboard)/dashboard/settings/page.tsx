0fe345911263b876a801d531b3393ed620ddefc9
ocque41 <73175248+ocque41@users.noreply.github.com>
2025-02-06 03:40:45 +0100

    feat: Update SettingsPage to fetch team data asynchronously

--- app/(dashboard)/dashboard/settings/page.tsx
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -1,10 +1,12 @@
 import Activity from '../activity/page';
 import General from '../general/page';
 import Security from '../security/page';
-import { TeamDataWithMembers } from '@/lib/db/schema';
 import { InviteTeamMember } from '../invite-team';
+import { TeamDataWithMembers } from '@/lib/db/schema';
+import { getTeamData } from '../settings';
 
-export default function SettingsPage({ teamData }: { teamData: TeamDataWithMembers }) {
+export default async function SettingsPage() {
+  const teamData: TeamDataWithMembers = await getTeamData();
   return (
     <div className="p-4 lg:p-8">
       <h1 className="text-lg lg:text-2xl font-medium mb-6">Settings</h1>
