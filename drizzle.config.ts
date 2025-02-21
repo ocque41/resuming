0fc498116c1b9068dad87611c6d6390f2c5c966d
ocque41 <73175248+ocque41@users.noreply.github.com>
2025-02-21 13:40:27 +0100

    /

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,5 @@ import { parse } from 'pg-connection-string';
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
+  connectionString: process.env.POSTGRES_URL!,
 } satisfies Config;
