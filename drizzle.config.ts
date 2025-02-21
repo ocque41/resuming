621bfbcc4387946f5d6efaab58ea4907c1d3e1a7
ocque41 <73175248+ocque41@users.noreply.github.com>
2025-02-21 13:39:50 +0100

    feat: Update database configuration to use structured credentials object

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,5 +4,12 @@ import { parse } from 'pg-connection-string';
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
-  databaseUrl: process.env.POSTGRES_URL,
+  dbCredentials: {
+    host: parse(process.env.POSTGRES_URL!).host!,
+    port: parseInt(parse(process.env.POSTGRES_URL!).port || '5432', 10),
+    user: parse(process.env.POSTGRES_URL!).user!,
+    password: parse(process.env.POSTGRES_URL!).password,
+    database: parse(process.env.POSTGRES_URL!).database!,
+    ssl: true,
+  },
 } satisfies Config;
