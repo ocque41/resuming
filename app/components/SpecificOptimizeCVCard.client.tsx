46e39f9a15ced7c6410cb18f24de889fafaa5ea2
ocque41 <73175248+ocque41@users.noreply.github.com>
2025-03-13 03:14:51 +0100

    Create SpecificOptimizeCVCard.client.tsx

---
+++ app/components/SpecificOptimizeCVCard.client.tsx
@@ -0,0 +1,12 @@
+/* use client */
+'use client';
+
+import React from 'react';
+
+export default function SpecificOptimizeCVCard() {
+  return (
+    <div className="p-4 bg-gray-800 text-white rounded-md">
+      <p>Specific Optimize CV Card - placeholder component.</p>
+    </div>
+  );
+} 
